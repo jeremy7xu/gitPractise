8e6ec0ea4758588c3d05b190685722c63e471732
Jeremy <1114371671@qq.com>
2020-09-03 09:04:13 +0800

    3

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -5,4 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println(132);
+    }
 }
